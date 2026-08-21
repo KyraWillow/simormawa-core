fedf9d8a1a9b498623b2e36a65c8dc3ee39223c1
dhan <kyrawillowxd@gmail.com>
2026-08-22 06:38:52 +0700

    Memperbaiki relasi `proker_id` menjadi one-to-one

--- backend/src/db/migrations/1787329758975_create-kak-table.sql
+++ backend/src/db/migrations/1787329758975_create-kak-table.sql
@@ -1,7 +1,7 @@
 -- Up Migration
 CREATE TABLE kak (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    proker_id uuid NOT NULL REFERENCES proker(id) ON DELETE RESTRICT,
+    proker_id uuid NOT NULL REFERENCES proker(id) ON DELETE RESTRICT UNIQUE,
     total_anggaran NUMERIC(11, 2) NOT NULL,
     status_kak status_kak_enum NOT NULL DEFAULT 'pengajuan',
     tanggal_diajukan TIMESTAMP NOT NULL DEFAULT NOW()
